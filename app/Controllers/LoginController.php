5bbb26289c4c58a823de3a4e750f2f0caf6b898e
Govind yadav <govindsvyadav@gmail.com>
2023-11-27 00:42:33 +0530

    Controllers setup

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace App\Classes ;
+namespace App\Controllers ;
 
-class Login
+class LoginController
 {
 	public function index(): string
 	{
---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+require __DIR__.'/../vendor/autoload.php';	
+
+session_start();
+
+$router = new App\Router() ;
+
+$router->get('/', [App\Controllers\HomeController::class, 'index'])
+		->get('/login', [App\Controllers\LoginController::class, 'index'])
+        ->post('/login/auth', [App\Controllers\LoginController::class, 'login']);
+
+echo $router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));
